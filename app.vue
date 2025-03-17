adc0586d36bf98abe6da2a030b203496f1fdad81
Kylin <107255387+kylin22@users.noreply.github.com>
2025-03-17 18:49:32 +1100

    chore: initial commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script lang="ts">
+  useHead({
+    title: "Stereographic Projection",
+    meta: [
+      { name: "description", content: "An interactive visualisation of stereographic projection and the Riemann sphere." }
+    ],
+  })
+</script>
+
+<style lang="scss">
+
+</style>
